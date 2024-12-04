bfd25d8a4a542a2fc4263b54010e9e5244335990
helmyfadlail <helmyfadlail.5@gmail.com>
2024-12-04 21:44:29 +0700

    refactor: change language in services home and edit the style

--- src/layouts/home/services.tsx
+++ src/layouts/home/services.tsx
@@ -17,7 +17,7 @@ export const Services = () => {
         return (
           <div
             key={index}
-            className="px-8 pt-24 space-y-8 text-center duration-300 border-2 md:pt-32 hover:pt-16 min-h-400 hover:bg-primary"
+            className="px-8 pt-24 text-center duration-300 border-2 md:pt-28 hover:pt-16 min-h-400 hover:bg-primary"
             onMouseEnter={() => setImgHover(index)}
             onMouseLeave={() => setImgHover(null)}
           >
@@ -26,9 +26,9 @@ export const Services = () => {
               alt={item.title}
               className={`mx-auto ${imgHover === index ? "size-12 sm:size-14 md:size-16" : "size-14 sm:size-16 md:size-20"}`}
             />
-            <h3 className={`font-semibold ${imgHover === index ? "text-lg sm:text-xl xl:text-2xl" : "text-xl sm:text-2xl xl:text-3xl"}`}>{item.title}</h3>
+            <h3 className={`font-semibold mt-8 ${imgHover === index ? "text-lg sm:text-xl xl:text-2xl" : "text-xl sm:text-2xl xl:text-3xl"}`}>{item.title}</h3>
 
-            <p className={`text-xs lg:text-sm transition-all duration-300 ${imgHover === index ? "h-auto opacity-100" : "h-0 opacity-0 "}`}>{item.description}</p>
+            <p className={`text-xs lg:text-sm transition-all duration-300 mt-4 ${imgHover === index ? "h-auto opacity-100" : "h-0 opacity-0 "}`}>{item.description}</p>
           </div>
         );
       })}
